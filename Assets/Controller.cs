849566328b4b1e58e4767f2b8352e2f554732792
Jack Carroll <boomkiller1119@gmail.com>
2018-11-06 15:52:59 +0800

    Initial commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class Controller : MonoBehaviour {
+
+    public float worldSpeed = 10f;
+    public GameObject world;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            Walk(false);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            Walk(true);
+        }
+        else
+        {
+            gameObject.GetComponent<Animator>().ResetTrigger("Walk");
+        }
+    }
+
+    private void Walk(bool left)
+    {
+        if (left)
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+            transform.localPosition = transform.position = new Vector3(14,0,0);
+            world.transform.Rotate(Vector3.forward * worldSpeed);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+            world.transform.Rotate(Vector3.forward * -worldSpeed);
+
+            transform.localPosition = transform.position = new Vector3(0, 0, 0);
+        }
+
+        gameObject.GetComponent<Animator>().SetTrigger("Walk");
+    }
+
+}
